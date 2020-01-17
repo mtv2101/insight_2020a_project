4a67345381c69ee3ad16347cef4dd966ed710eb4
matt_valley <valley@gmail.com>
2020-01-17 13:10:38 -0800

    import_pdf created, tagger created

---
+++ RealCrowd_Insights/tagger.py
@@ -0,0 +1,14 @@
+
+
+
+def get_keywords(self):
+    self.test_keywords = ['construction date', 'mortgage amount', 'maintenance cost']
+
+
+def get_tags():
+    self.get_keywords()
+
+
+def match_tags(text, tags):
+    for t, tag in enumerate(tags):
+        matches = ...
