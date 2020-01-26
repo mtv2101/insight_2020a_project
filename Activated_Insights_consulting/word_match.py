f49d58710d728b55db78462315e9b854a70ce557
matt_valley <valley@gmail.com>
2020-01-25 17:00:02 -0800

    finish first-draft of regex matching

--- Activated_Insights_consulting/word_match.py
+++ Activated_Insights_consulting/word_match.py
@@ -32,56 +32,93 @@ class word_match(object):
 
     def setup_match_rules(self):
 
-        {'resident': [{"TEXT": {"REGEX": "^[Rr](es.?d.?nt)$"}}],
-         'patient': [{"TEXT": {"REGEX": "^[Pp](at\.?\.?nt)$"}}]}
+        self.topics = {'Co-workers/teamwork':   [{"TEXT": {"REGEX": "^[Cc](ol.?.?.?g.?e.?)$"}}, #colleague
+                                                {"TEXT": {"REGEX": "^[Cc](o.?w.?.?k.?er.?)$"}}, #coworker
+                                                {"TEXT": {"REGEX": "^[Aa](s.?o.?.?.?te.?)$"}}, #associate
+                                                {"TEXT": {"REGEX": "^[Tt](eam.?.?.?te.?)$"}}, #teammate
+                                                {"TEXT": {"REGEX": "^[Tt](eam.?\work)$"}}, #teamwork
+                                                {"TEXT": {"REGEX": "^[Ss](taf.?.?)$"}}, #staff
+                                                {"TEXT": {"REGEX": "^[Ff](amily)$"}}, #family
+                                                {"TEXT": {"REGEX": "^[Cc](ommunity)$"}} #community,
+                                                ],
+                  'Schedule':                   [{"TEXT": {"REGEX": "^[Ss](.?.?.?dule.?)$"}}, #schedule,
+                                                {"TEXT": {"REGEX": "^[Bb](usy)$"}}, #busy',
+                                                {"TEXT": {"REGEX": "^[Cc](al.?nd.?r.?)$"}}  #calendar
+                                                ],
+                  'Management':                 [{"TEXT": {"REGEX": "^[Ss](upervis.?.?.?)$"}}, #'supervisor',
+                                                {"TEXT": {"REGEX": "^[Bb](oss.?.?)$"}}, #'boss',
+                                                {"TEXT": {"REGEX": "^[Rr](ule.?)$"}}, #'rule',
+                                                {"TEXT": {"REGEX": "^[Mm](an.?g.?ment)$"}}, #'management',
+                                                {"TEXT": {"REGEX": "^[Aa](dmin.?.?.?.?.?.?.?.?.?.?)$"}} #'administration'
+                                                ],
+                  'Benefits and leave':         [{"TEXT": {"REGEX": "^[Bb](enefit.?)$"}}, #'benefits',
+                                                {"TEXT": {"REGEX": "^[Vv](ac.?.?.?on.?)$"}}, #'vacation',
+                                                {"TEXT": {"REGEX": "^[Tt](ime.?.?off.?)$"}}, #'time off',
+                                                {"TEXT": {"REGEX": "^[Pp](erson.?.?.?time)$"}}, #'personal time',
+                                                {"TEXT": {"REGEX": "^[Ss](ick.?day.?)$"}} #sick days
+                                                ],
+                  'Support and resources':      [{"TEXT": {"REGEX": "^[Ss](upport)$"}}, #'support',
+                                                {"TEXT": {"REGEX": "^[Rr](eso.?rc.?.?.?)$"}} #resources, resourcing
+                                                ],
+                  'Customers':                  [{"TEXT": {"REGEX": "^[Rr](es.?d.?nt.?)$"}}, # resident,
+                                                {"TEXT": {"REGEX": "^[Pp](at.?.?nt.?)$"}}, # patient
+                                                {"TEXT": {"REGEX": "^[Ss](enior.?)$"}} # senior
+                                                ],
+                  'Pay':                        [{"TEXT": {"REGEX": "^[Mm](oney)$"}}, #'money',
+                                                {"TEXT": {"REGEX": "^[Pp](ay.?.?.?.?.?)$"}}, #'pay', paycheck, payment
+                                                {"TEXT": {"REGEX": "^[Rr](es.?d.?nt.?)$"}}, #'end of the month',
+                                                {"TEXT": {"REGEX": "^[Dd](ebt)$"}}, #'debt',
+                                                {"TEXT": {"REGEX": "^[Ll](oan.?)$"}} # loan
+                                                ],
+                  'Recognition':                [{"TEXT": {"REGEX": "^[Rr](ecognition.?)$"}}, #'recognition',
+                                                {"TEXT": {"REGEX": "^[Aa](p.?rec.?.?.?tion)$"}} #'appreciation'],
+                                                ],
+                  'Learning & Development':     [{"TEXT": {"REGEX": "^[Ss](taf.?.?[Dd]evel.?.?ment)$"}}, #'Staff development',
+                                                {"TEXT": {"REGEX": "^[Tt](each.?.?.?.?)$"}}, #'teach', teaching, teacher
+                                                {"TEXT": {"REGEX": "^[Gg](rowth)$"}}, #'growth',
+                                                {"TEXT": {"REGEX": "^[Tt](ra.?.?ing)$"}} #training
+                                                ],
+                  'Purpose':                    [{"TEXT": {"REGEX": "^[Pp](ur.?p.?.?se)$"}}, #'purpose',
+                                                {"TEXT": {"REGEX": "^[Mm](eaning)$"}}, #'meaning',
+                                                {"TEXT": {"REGEX": "^[Mm](ission)$"}}, #'mission'],
+                                                ],
+                  'No answer/Nothing':          [],
+                  'Location to home':           [{"TEXT": {"REGEX": "^[Cc](om.?ute)$"}}, #'commute',
+                                                {"TEXT": {"REGEX": "^[Dd](riving)$"}}, #'driving',
+                                                {"TEXT": {"REGEX": "^[Ll](ocation)$"}} # location'
+                                                ],
+                  'Staffing level':             [{"TEXT": {"REGEX": "^[Hh](ead.?count)$"}}, #'head-count',
+                                                {"TEXT": {"REGEX": "^[Ss](taf.?.?.?.?)$"}}, #'staffing',
+                                                {"TEXT": {"REGEX": "^[Hh](ire)$"}}, #'hire',
+                                                {"TEXT": {"REGEX": "^[Ee](mployment)$"}}, #'employment'],
+                                                ],
+                  'Communication':              [{"TEXT": {"REGEX": "^[Ll](is.?en)$"}}, #'listen',
+                                                {"TEXT": {"REGEX": "^[Cc](om.?un.?cat.?.?.?.?)$"}} #'communicate, communication
+                                                ],
+                  'Quality of care':            [{'TEXT': {'REGEX': "^[Cc](are)$"}}], #care AND residents,
+                  'Employee relations':         [{"TEXT": {"REGEX": "^[Pp](roblem.?)$"}}, #'problem',
+                                                {"TEXT": {"REGEX": "^[Hh](ar.?as.?.?.?.?.?)$"}}, #'harassment',
+                                                {"TEXT": {"REGEX": "^[Aa](bus.?.?.?)$"}}, #'abuse, abusing
+                                                {"TEXT": {"REGEX": "^[Ll](awyer)$"}}, #'lawyer',
+                                                {"TEXT": {"REGEX": "^[Tt](rouble)$"}}, #'trouble',
+                                                {"TEXT": {"REGEX": "^[Ss](ex)$"}}, #'sex',
+                                                {"TEXT": {"REGEX": "^[Rr](ace)$"}}, #'race',
+                                                {"TEXT": {"REGEX": "^[Gg](ender)$"}}, #'gender',
+                                                {"TEXT": {"REGEX": "^[Bb](igot.?.?.?)$"}}, #'bigot',
+                                                {"TEXT": {"REGEX": "^[Ig](nor.?.?.?)$"}}, #'ignorant', ignore
+                                                {"TEXT": {"REGEX": "^(.?.?legal.?)$"}}, #'illegal', 'legal'
+                                                {"TEXT": {"REGEX": "^[Cc](ollapse)$"}}, #'collapse',
+                                                {"TEXT": {"REGEX": "^[Ii](nterven.?.?.?.?)$"}}, #'intervention', intervene
+                                                ],
+                  'Facility/setting':           [{"TEXT": {"REGEX": "^[Ff](ac.?l.?t.?.?.?)$"}}, #'facilities',
+                                                {"TEXT": {"REGEX": "^[Mm](a.?nt.?n.?nce)$"}}, #'maintenance',
+                                                {"TEXT": {"REGEX": "^[Cc](lean.?.?.?)$"}}, #'clean', cleanly, cleaning
+                                                {"TEXT": {"REGEX": "^(.?.?hygen.?.?)$"}} #'hygenic', unhygenic
+                                                ]
+                      }
+
 
     def match_seeds(self):
-
-        self.topics = {'Co-workers/teamwork': [{"TEXT": {"REGEX": "^[Cc](ol.?.?.?g.?e.?)$"}}, #colleague
-                                               {"TEXT": {"REGEX": "^[Cc](o.?w.?.?k.?er.?)$"}}, #coworker
-                                               {"TEXT": {"REGEX": "^[Aa](s.?o.?.?.?te.?)$"}}, #associate
-                                               {"TEXT": {"REGEX": "^[Tt](eam.?.?.?te.?)$"}}, #teammate
-                                               {"TEXT": {"REGEX": "^[Tt](eam.?\work)$"}}, #teamwork
-                                               {"TEXT": {"REGEX": "^[Ss](taf.?.?)$"}}, #staff
-                                               {"TEXT": {"REGEX": "^[Ff](amily)$"}}, #family
-                                               {"TEXT": {"REGEX": "^[Cc](ommunity)$"}} #community,
-                                             ],
-                  'Schedule': [{"TEXT": {"REGEX": "^[Ss](.?.?.?dule.?)$"}}, #schedule,
-                               {"TEXT": {"REGEX": "^[Bb](usy)$"}}, #busy',
-                               {"TEXT": {"REGEX": "^[Cc](al.?nd.?r.?)$"}}  #calendar
-                               ],
-                  'Management': [{"TEXT": {"REGEX": "^[Ss](upervis.?.?.?)$"}}, #'supervisor',
-                                 {"TEXT": {"REGEX": "^[Bb](oss.?.?)$"}}, #'boss',
-                                 {"TEXT": {"REGEX": "^[Rr](ule.?)$"}}, #'rule',
-                                 {"TEXT": {"REGEX": "^[Mm](an.?g.?ment)$"}}, #'management',
-                                 {"TEXT": {"REGEX": "^[Aa](dmin.?.?.?.?.?.?.?.?.?.?)$"}} #'administration'
-                                 ],
-                  'Benefits and leave': ['benefits', 'vacation', 'time off', 'personal time', 'sick days'],
-                  'Support and resources': ['support', 'resources'],
-                  'Customers': ['resident', 'patient', 'senior'],
-                  'Pay': ['money', 'pay', 'paycheck', 'end of the month', 'debt', 'loans'],
-                  'Recognition': ['recognition', 'appreciation'],
-                  'Learning & Development': ['Staff development', 'teach', 'growth', 'training'],
-                  'Purpose': ['purpose', 'meaning', 'mission'],
-                  'No answer/Nothing': [],
-                  'Location to home': ['commute', 'drive', 'location'],
-                  'Staffing level': ['head-count', 'staffing', 'hire', 'employment'],
-                  'Communication': ['listen', 'communicate'],
-                  'Quality of care': ['care AND residents'],
-                  'Employee relations': ['problem','harassment','abuse','lawyer','trouble','identity','sex','race','gender','bigot','ignorant','ignore','illegal','collapse','union','intervention'],
-                  'Facility/setting': ['facilities','maintenance','clean', 'hygenic']
-                      }
-
-        '''
-        self.seeds = {'Residents': ['resident', 'patient', 'senior'],
-                      'Communication': ['listen', 'communicate', 'complaint'],
-                      'Management': ['supervisor', 'boss', 'rule', 'management', 'administration'],
-                      'Scheduling': ['schedule', 'busy'],
-                      'Compensation': ['compensation', 'money', 'pay', 'benefit'],
-                      'Colleagues': ['colleague', 'employee', 'peer', 'coworker', 'associate', 'teammate'],
-                      }
-        '''
-
         # apply spacy matcher
         if self.match_method == 'spacy':
             # setup spacy matcher
