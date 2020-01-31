8c253537ae118c25d1073716381d0092d4ed656b
matt_valley <valley@gmail.com>
2020-01-31 00:18:46 -0800

    many bugfixes, functional version

--- Activated_Insights_consulting/tri_training.py
+++ Activated_Insights_consulting/tri_training.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pandas as pd
+
 import os
 import timeit
 import matplotlib.pyplot as plt
 from random import choices
+from numpy.random import default_rng
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -75,7 +77,7 @@ def setup_pipes():
                                                             n_jobs=-1))])
 
     #MLP_pipeline = Pipeline([('clf', OneVsRestClassifier(MLPClassifier(), n_jobs=-1))])
-    KNN_pipeline = Pipeline([('clf', OneVsRestClassifier(KNeighborsClassifier(weights='distance')
+    KNN_pipeline = Pipeline([('clf', OneVsRestClassifier(KNeighborsClassifier(weights='uniform')
                                                          , n_jobs=-1))])
 
     return [LogReg_pipeline, Forest_pipeline, KNN_pipeline]
@@ -105,121 +107,102 @@ def generic_fit(model, X_train, y_train, X_test, y_test, X_ul):
     model.fit(X_train, y_train)
     predictions = model.predict(X_ul)
 
-    process_time = timeit.default_timer() - start_time
-    print(str(model) + ' training and predictions took: ' + str(process_time) + ' seconds')
-
     # calculate metrics
     y_pred = model.predict(X_test)
-    print('accuracy = ' + str(metrics.accuracy_score(y_test, y_pred)))
-    print('macro precision = ' + str(metrics.precision_score(y_test, y_pred,
-                                                             average='macro')))
-    print('recall score = ' + str(metrics.recall_score(y_test, y_pred,
-                                                       average='macro')))
+    acc = metrics.accuracy_score(y_test, y_pred)
+    prec = metrics.precision_score(y_test, y_pred, average='samples')
+    rec = metrics.recall_score(y_test, y_pred,average='macro')
+    print('accuracy = ' + str(acc))
+    print('macro precision = ' + str(prec))
+    print('recall score = ' + str(rec))
     print("\n")
 
     process_time = timeit.default_timer() - start_time
     print(str(model) + ' training and predictions took: ' + str(process_time) + ' seconds')
 
-    return predictions
+    return predictions, acc, prec, rec
 
 
-def find_consensus(predictions, training_dat, X_ul):
+def find_consensus(preds, training_dat, training_labels, X_ul):
 
-    X0, X1, X2 = zip(training_dat)
+    X0, X1, X2 = training_dat
+    y0, y1, y2 = training_labels
 
-    p1, p2, p3 = zip(predictions)
-    ensemble = np.vstack([p1, p2, p3])
-    print(ensemble.shape)
+    assert (X0.shape[0] == y0.shape[0])
+    assert (X1.shape[0] == y1.shape[0])
+    assert (X2.shape[0] == y2.shape[0])
 
-    X0_newdata = []
-    X1_newdata = []
-    X2_newdata = []
-    consensus = np.mean(ensemble, axis=3)
+    p1, p2, p3 = preds
+    ensemble = np.stack([p1, p2, p3], axis=0).astype('int16')
+
+    consensus = np.mean(ensemble.astype('float'), axis=2)
+    print(consensus.shape)
     for i in range(consensus.shape[1]):
-        if (consensus[0, i] != consensus[1, i]) and (consensus[0, i] != consensus[2, i]) and (
+
+        # if model 0 is the odd-one-out
+        if (consensus[0, i] != consensus[1, i]) and (
+                consensus[0, i] != consensus[2, i]) and (
                 consensus[1, i] == consensus[2, i]):
-            X0_newdata.append(X_ul[i, :])
-        elif (consensus[1, i] != consensus[0, i]) and (consensus[1, i] != consensus[2, i]) and (
+            X0 = np.vstack([X0, X_ul[i, :]])
+            y0 = np.vstack([y0, ensemble[1, i, :].squeeze()])
+
+        # if model 1 is the odd-one-out
+        elif (consensus[1, i] != consensus[0, i]) and (
+                consensus[1, i] != consensus[2, i]) and (
                 consensus[0, i] == consensus[2, i]):
-            X1_newdata.append(X_ul[i, :])
-        elif (consensus[2, i] != consensus[0, i]) and (consensus[2, i] != consensus[1, i]) and (
+            X1 = np.vstack([X1, X_ul[i, :]])
+            y1 = np.vstack([y1, ensemble[0, i, :].squeeze()])
+
+        # if model 2 is the odd-one-out
+        elif (consensus[2, i] != consensus[0, i]) and (
+                consensus[2, i] != consensus[1, i]) and (
                 consensus[1, i] == consensus[0, i]):
-            X2_newdata.append(X_ul[i, :])
+            X2 = np.vstack([X2, X_ul[i, :]])
+            y2 = np.vstack([y2, ensemble[1, i, :].squeeze()])
 
-    X0 = np.vstack([X0, np.array(X0_newdata)])
-
-    X1 = np.vstack([X1, np.array(X1_newdata)])
-
-    X2 = np.vstack([X2, np.array(X2_newdata)])
+    print(X0.shape, X1.shape, X2.shape)
+    print(y0.shape, y1.shape, y2.shape)
 
     training_dat = [X0, X1, X2]
+    training_labels = [y0, y1, y2]
 
-    return training_dat
+    return training_dat, training_labels
 
 
 
 
 def tri_fit(X,y,X_ul,models,predict_size=1000):
 
-    num_folds = 3
+    num_folds = 10
 
     X = preprocessing.scale(X)
     X_ul = preprocessing.scale(X_ul)
-    X_train, X_test, y_train, y_test = train_test_split(X,y, random_state=42,test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42,test_size=0.2)
 
     # initialize three copies of training data before accumulating model-specific examples
     training_dat = [X_train, X_train, X_train]
+    training_labels = [y_train, y_train, y_train]
+
+    all_acc = []
+    all_prec = []
+    all_rec = []
 
     for n in range(num_folds):
 
-        print(X_train.shape, y_train.shape, X_test.shape, y_test.shape, X_ul.shape, predict.shape)
+        rng = default_rng()
+        rand_idx = rng.choice(np.arange(0,X_ul.shape[0], size=10000, replace=False))
+        X_ul_sampled = X_ul[rand_idx, :]
 
         preds = []
         for m,model in enumerate(models):
-            X_train = model_dat[m]
-            preds.append(generic_fit(model, X_train, y_train, X_test, y_test))
-
-        model_dat = find_consensus(preds, training_dat, X_ul)
-
-
-
-
-
-
-@staticmethod
-def train_test(self, model):
-
-    #X = self.tfidf(self.ul_df)
-    X = self.tfidf(self.l_df)
-    y = self.l_df['JK label'].values
-    print(len(self.ul_df), len(self.l_df))
-    print(X.shape, y.shape)
-
-    self.init_model(model)
-
-    skf = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
-
-    for train_index, test_index in skf.split(X, y):
-        X_train, X_test = X[train_index], X[test_index]
-        y_train, y_test = y[train_index].T, y[test_index].T
-
-        self.logit.fit(X_train, y_train)
-        y_pred = self.logit.predict((X_test))
-
-        print('accuracy = ' + str(metrics.accuracy_score(y_test, y_pred)))
-        print('balenced accuracy = ' + str(metrics.balanced_accuracy_score(y_test, y_pred)))
-        print('macro precision = ' + str(metrics.precision_score(y_test, y_pred, average='macro')))
-
-
-def init_model(self, model = 'logit'):
-
-    if model == 'logit':
-        self.logit = LogisticRegression(class_weight='balanced',
-                                   random_state=42,
-                                   multi_class='multinomial',
-                                   verbose=1,
-                                   max_iter=1000)
-
+            X_train = training_dat[m]
+            y_train = training_labels[m]
+            pred, acc, prec, rec = generic_fit(model, X_train, y_train, X_test, y_test, X_ul_sampled)
+            preds.append(pred)
+            all_acc.append(acc)
+            all_prec.append(prec)
+            all_rec.append(rec)
+        training_dat, training_labels = find_consensus(preds, training_dat, training_labels, X_ul_sampled)
 
 
 if __name__ == "__main__":
